4f3c606b1df3f0cb19910cc7c3dc8df650835db2
Tuxi <one77r@gmail.com>
2026-01-12 20:52:15 +0800

    feat: bump ver

--- wxt.config.ts
+++ wxt.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   manifest: {
     name: 'YFSP Ad Blocker',
     description: '自动跳过 yfsp.tv 视频广告',
-    version: '0.0.1',
+    version: '0.0.2',
     permissions: ['storage'],
     host_permissions: ['*://*.yfsp.tv/*'],
   },
